18880925b238686b0b558c0bba5818e77c9cf202
unknown <minh.huang96@gmail.com>
2019-07-22 14:31:41 -0700

    finished function-conversion's stretch challenges

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -44,3 +44,9 @@
 //   return num * 3;
 // });
 // console.log(triple);
+console.log("Stretch Challenge:");
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map( (num)=> {
+  return num * 3;
+});
+console.log(triple);
